932e256f02836186a7523d9176a2c9f0cef96624
Povilas Lekavicius <povilasleka@gmail.com>
2026-08-23 23:21:29 +0300

    chore: move create-migration script to shared/scripts/drizzle

--- apps/delivery/src/index.ts
+++ apps/delivery/src/index.ts
@@ -1,12 +1,12 @@
 import express, { type Express, type Request, type Response } from 'express';
-import { buildDrizzleClient } from './infrastructure/postgres/client.ts';
-import { getConfig } from './config/config.ts';
+//import { buildDrizzleClient } from './infrastructure/postgres/client.ts';
+//import { getConfig } from './config/config.ts';
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-const config = getConfig();
-const drizzleClient = buildDrizzleClient(config.databaseUrl);
+//const config = getConfig();
+//const drizzleClient = buildDrizzleClient(config.databaseUrl);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
